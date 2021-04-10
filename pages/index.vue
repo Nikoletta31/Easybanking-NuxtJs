865cae02e5ee22fb5d456933a8f4365037d74658
Nika - BC <nika1kharebava@gmail.com>
2021-04-10 21:38:38 +0200

    navbar

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+     <NavBar/>
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue"
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+padding:0;
+}
+
+.title {
+font-family: 'Public Sans', sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
